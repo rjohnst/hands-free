1fcb08197a954db845e4d77018ae53b4f03c6f9d
Rob Johnston <rob@rjohnst.com>
2014-07-29 23:23:44 +1000

    adapted add-credential so you can create a ssh credentials as well

--- src/main/java/org/jenkinsci/plugins/hands_free/credentials/AddCredentialCommand.java
+++ src/main/java/org/jenkinsci/plugins/hands_free/credentials/AddCredentialCommand.java
@@ -1,14 +1,17 @@
 package org.jenkinsci.plugins.hands_free.credentials;
 
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey;
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey.DirectEntryPrivateKeySource;
+import com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey.PrivateKeySource;
 import com.cloudbees.plugins.credentials.Credentials;
 import com.cloudbees.plugins.credentials.CredentialsScope;
 import com.cloudbees.plugins.credentials.CredentialsStoreAction;
 import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
 import hudson.AbortException;
 import hudson.Extension;
+import hudson.util.IOUtils;
 import org.kohsuke.args4j.Argument;
-
-import java.io.PrintStream;
+import org.kohsuke.args4j.Option;
 
 /**
  * Adds an extension to the credentials plugin to allow creation of new credentials from the command line
@@ -20,17 +23,21 @@ public class AddCredentialCommand extends AbstractCredentialCommand {
 
     @Override
     public String getShortDescription() {
-        return "Adds a new global username and password credential";
+        return "Adds a new global credential. Username and password is default, set --ssh and put a private key file " +
+                "on stdin for an SSH with inline key credential.";
     }
 
-    @Argument(metaVar="USERNAME",index=0,usage="The username that identifies the credential",required=true)
+    @Argument(metaVar="DESCRIPTION",index=0,usage="A meaningful description of the credential to add",required=true)
+    public String description;
+
+    @Argument(metaVar="USERNAME",index=1,usage="The username that identifies the credential",required=true)
     public String username;
 
-    @Argument(metaVar="PASSWORD",index=1,usage="The password that authenticates the credential",required=true)
-    public String password;
+    @Argument(metaVar="PASSWORD",index=2,usage="The password that authenticates the credential")
+    public String password = "";
 
-    @Argument(metaVar="DESCRIPTION",index=2,usage="A meaningful description of the credential to add",required=true)
-    public String description;
+    @Option(name="--ssh", usage="Set to create an SSH username and inline key credential")
+    public boolean doSsh = false;
 
     @Override
     protected int run() throws Exception {
@@ -41,21 +48,20 @@ public class AddCredentialCommand extends AbstractCredentialCommand {
 
         wrapper.getStore().checkPermission(CredentialsStoreAction.CREATE);
 
-        Credentials credentials = new UsernamePasswordCredentialsImpl(
-                CredentialsScope.GLOBAL, null, description, username, password
-        );
+        Credentials credentials;
+        if (doSsh) {
+            PrivateKeySource source = new DirectEntryPrivateKeySource(IOUtils.toString(stdin));
+            credentials = new BasicSSHUserPrivateKey(CredentialsScope.GLOBAL, null, username, source, password, description);
+
+        } else {
+            credentials = new UsernamePasswordCredentialsImpl(
+                    CredentialsScope.GLOBAL, null, description, username, password
+            );
+        }
 
         wrapper.getStore().addCredentials(wrapper.getDomain(), credentials);
 
         return 0;
     }
 
-    @Override
-    protected void printUsageSummary(PrintStream stderr) {
-        stderr.println(
-            "Adds a new global credential, using a username and password for authentication.\n" +
-            "Calls to this command should take the form:\n" +
-            "\tadd-credential username password \"some description\""
-        );
-    }
 }
